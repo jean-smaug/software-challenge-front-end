f22ddb1f940537ebecfce169db56f05f6635d60a
maxime blanc <maxime.blanc@m6.fr>
2019-06-27 12:42:52 +0200

    style: use eslint to format code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, {Component} from 'react';
-import './App.css';
-import ScanContainer from "./ScanContainer";
+import React, { Component } from 'react'
+import './App.css'
+import ScanContainer from './ScanContainer'
 
 class App extends Component {
-
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
+  render () {
+    return (
+      <div className='App'>
+        <header className='App-header'>
                     Software Challenge
-                </header>
-                <ScanContainer />
-            </div>
-        );
-    }
+        </header>
+        <ScanContainer />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/ScanContainer.js
+++ src/ScanContainer.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import ScanList from './ScanList';
-import {createScanData, createUserData} from './data'
-
+import React from 'react'
+import ScanList from './ScanList'
+import { createScanData, createUserData } from './data'
 
 class ScanContainer extends React.Component {
-
     state = {
-        scans: createScanData(),
-        users: createUserData(),
+      scans: createScanData(),
+      users: createUserData()
     };
 
-    render() {
-        return (
-            <div>
-                <ScanList
-                    scans={this.state.scans}
-                    users={this.state.users}
-                />
-            </div>
-        );
+    render () {
+      return (
+        <div>
+          <ScanList
+            scans={this.state.scans}
+            users={this.state.users}
+          />
+        </div>
+      )
     }
 }
 
-export default ScanContainer;
+export default ScanContainer
